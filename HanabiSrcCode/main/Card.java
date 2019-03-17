40d6407ba534850acdd386ecbc8fa7bd0f41e0b2
Joel <jms676@usask.ca>
2019-03-17 15:58:36 -0600

    Add tests for Card class, added functions and comments to Card class

--- HanabiSrcCode/main/Card.java
+++ HanabiSrcCode/main/Card.java
@@ -5,8 +5,11 @@ public class Card {
     private Boolean numberKnown;    // marker for whether the rank of the card is known
 
     public Card(char colour, char number);
-        // object contstructor, takes color and number value and sets both
+        // object constructor, takes color and number value and sets both
         // colourKnown and numberKnown to false.
+
+    public Card();
+        // alternate constructor for player's own cards
 
     public void numberGiven();
         // sets numberKnown to true.
@@ -14,7 +17,7 @@ public class Card {
     public void colourGiven();
         // sets colourKnown to true.
 
-    public int getNumber();
+    public char getNumber();
         // returns the ranks of the card.
 
     public char getColour();
@@ -26,4 +29,13 @@ public class Card {
     public Boolean checkNumberKnown();
         // returns whether the colour of the card has been given as information.
 
+    public void setNumber(char number);
+        // sets the number of the card to the given value and
+        // marks the card as known
+        // throws an exception if the card already has a number
+
+    public void setColour(char colour);
+        // sets the colour of the card to the given value
+        // and marks the card as known
+        // throws an exception if the card already has a colour
 }
--- HanabiSrcCode/test/CardTest.java
+++ HanabiSrcCode/test/CardTest.java
@@ -6,26 +6,79 @@ class CardTest {
 
     @Test
     void numberGiven() {
-
+        Card aCard = new Card('b', '2');
+        assertFalse(aCard.checkNumberKnown());
+        aCard.numberGiven();
+        assertTrue(aCard.checkNumberKnown());
     }
 
     @Test
     void colourGiven() {
+        Card bCard = new Card('g', '4');
+        assertFalse(bCard.checkColourKnown());
+        bCard.colourGiven();
+        assertTrue(bCard.checkColourKnown());
     }
 
     @Test
     void getNumber() {
+        Card cCard = new Card ('w', '1');
+        assertEquals('1', cCard.getNumber());
     }
 
     @Test
     void getColour() {
+        Card cCard = new Card ('r', '1');
+        assertEquals('r', cCard.getColour());
     }
 
     @Test
     void checkColourKnown() {
+        Card bCard = new Card('g', '4');
+        assertFalse(bCard.checkColourKnown());
+        bCard.colourGiven();
+        assertTrue(bCard.checkColourKnown());
     }
 
     @Test
     void checkNumberKnown() {
+        Card aCard = new Card('b', '2');
+        assertFalse(aCard.checkNumberKnown());
+        aCard.numberGiven();
+        assertTrue(aCard.checkNumberKnown());
+    }
+
+    @Test
+    void setNumber() {
+        Card nCard = new Card();
+        assertNull(nCard.getNumber());
+        assertFalse(nCard.checkNumberKnown());
+        nCard.setNumber('3');
+        assertTrue(nCard.checkNumberKnown());
+        assertEquals('3', nCard.getNumber());
+        try{
+            nCard.setNumber('1');
+            assertEquals('3', nCard.getNumber());
+        }
+        catch(Exception E){
+            assertEquals('3', nCard.getNumber());
+        }
+    }
+
+    @Test
+    void setColour() {
+        Card nCard = new Card();
+        assertNull(nCard.getColour());
+        assertFalse(nCard.checkColourKnown());
+        nCard.setColour('r');
+        assertTrue(nCard.checkColourKnown());
+        assertEquals('r', nCard.getColour());
+        try{
+            nCard.setColour('w');
+            assertEquals('r', nCard.getColour());
+        }
+        catch(Exception E){
+            assertEquals('r', nCard.getColour());
+        }
     }
 }
