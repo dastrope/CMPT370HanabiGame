1dea65cbaec37658f97c0ae820c49f81383981ae
Joel <jms676@usask.ca>
2019-03-17 16:47:47 -0600

    Add tests for Firework class, specified collection as HashMap for Firework class

--- HanabiSrcCode/main/Fireworks.java
+++ HanabiSrcCode/main/Fireworks.java
@@ -1,11 +1,18 @@
+import java.util.HashMap;
+
 public class Fireworks {
-    public Collection Fireworks;
+    public HashMap<String,Integer> Fireworks;
+    //a container for individual fireworks
 
     public Fireworks();
+    //initializes all fireworks with height set to 0 for each
 
     public void addFirework(char colour);
+    //increments a fierwork by 1 on its height
 
-    public Boolean checkBuildValid(Card tryme);
+    public Boolean checkBuildValid(Card tryMe);
+    //checks to see if a play is valid and a firework can be successfully increased
 
-    public Collection getFireworks();
+    public HashMap getFireworks();
+    //returns the collection of fireworks
 }
