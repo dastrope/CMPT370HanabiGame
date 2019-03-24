3091d912944af844e0e8cd279a3e4d6271b01b47
DAstrope <dja392@mail.usask.ca>
2019-03-24 11:08:44 -0600

    modified CardButton and main classes in ViewTesting to add documentation

--- HanabiSrcCode/main/CardButton.java
+++ HanabiSrcCode/main/CardButton.java
@@ -1,16 +1,29 @@
 import javafx.scene.control.Button;
 
 public class CardButton extends Button {
+    /**
+     * A simple extension of the button class to allow for suit/rank setting and image address generation for a card
+     */
     char suit;
     char rank;
+
     public CardButton(char suit, char rank){
+        /**
+         * Basic constructor at the moment just sets field values to those given.
+         */
         this.suit = suit;
         this.rank = rank;
     }
     public char getSuit(){
+        /**
+         * Returns the character in the card's suit field
+         */
         return this.suit;
     }
     public String getImageString(){
+        /**
+         * Generates an image address based on the suit and rank of the card, if not available sets it to the unknown card
+         */
         String address = "cards_set1/basic/";
         switch(suit){
             case 'b':
--- HanabiSrcCode/main/main.java
+++ HanabiSrcCode/main/main.java
@@ -28,17 +28,29 @@ public class main extends Application {
 
     @Override
     public void start(Stage stage) {
+        /*
+         * This is all just code for testing certain functionality, much of it is janky and should not be replicated
+         * for the final product. No states are present so things are always drawn/done the same way, and objects are all
+         * hardcoded in and completely exist only in the view itself, so of course that will require changing.
+         * Some of the event handlers will also need to be moved to controller, although for hovering events
+         * I don't think that is necessary at the moment, as those are entirely clientside view only changes.
+         * Optimization may also be possible for things like image drawing, should look at that
+         */
         Pane root_stack = new Pane() ;
 
+        /*create background and groups for buttons*/
         Scene scene = new Scene( root_stack, 1200, 800 ) ;
         scene.setFill( Color.LAWNGREEN ) ;
         root_stack.setBackground( null );
-
         Button button1   = new Button( "TEST1" ) ;
         Button  button2 = new Button( "TEST2" ) ;
-        HBox pane_for_buttons = new HBox( 16 ) ; // space between buttons is 16
-        HBox hand = new HBox(20);
+        HBox pane_for_buttons = new HBox( 16 ) ;
+        HBox hand = new HBox(20);           // space between buttons is 20
+
+        /*create a list to contain cards for tracking card position*/
         ArrayList<Button> cardList = new ArrayList<>();
+
+        /*loop to create all the cards with hardcoded information*/
         for (int i = 0 ; i<5 ; i++){
             char suit;
             char rank;
@@ -47,21 +59,20 @@ public class main extends Application {
             } else{
                 suit = 'r';
             }
-//            rank = Integer.toString(i);
-            rank = Character.forDigit(i+1, 10);
-            System.out.println(rank);
-            CardButton card1 = new CardButton(suit, rank);
-            ImageView unknown = new ImageView(new Image(card1.getImageString()));
-            unknown.setFitHeight(100);
-            unknown.setFitWidth(66);
-            card1.setGraphic(unknown);
+            rank = Character.forDigit(i+1, 10);         //convert rank to char
+            CardButton card1 = new CardButton(suit, rank);          //create a card
+
+            /*set the cards image and add the card to the cardlist*/
+            ImageView image = new ImageView(new Image(card1.getImageString()));
+            image.setFitHeight(100);
+            image.setFitWidth(66);
+            card1.setGraphic(image);
             cardList.add(card1);
+
+            /*set event for when the mouse hovers over the card*/
             card1.setOnMouseEntered(( MouseEvent event ) ->
             {
-                double clicked_point_x = event.getSceneX() ;
-                double clicked_point_y = event.getSceneY() ;
-                System.out.println(clicked_point_x + clicked_point_y);
-//                card1.setTranslateY(-15);
+                /*get all siblings of the card, raise any that match its suit*/
                 ObservableList<Node> cards = card1.getParent().getChildrenUnmodifiable();
                 for (Node card : cards){
                     CardButton cd = (CardButton) card;
@@ -70,12 +81,11 @@ public class main extends Application {
                     }
                 }
             });
+
+            /*set event for when the stops hovering over the card*/
             card1.setOnMouseExited(( MouseEvent event ) ->
             {
-                double clicked_point_x = event.getSceneX() ;
-                double clicked_point_y = event.getSceneY() ;
-                System.out.println(clicked_point_x + clicked_point_y);
-//                card1.setTranslateY(0);
+                /*get all siblings of the card, lower any that match its suit*/
                 ObservableList<Node> cards = card1.getParent().getChildrenUnmodifiable();
                 for (Node card : cards){
                     CardButton cd = (CardButton) card;
@@ -84,29 +94,33 @@ public class main extends Application {
                     }
                 }
             });
+
+            /*set event for clicking the card to generate a play event*/
             card1.setOnMouseClicked((MouseEvent event) ->
             {
-                double clicked_point_x = event.getSceneX() ;
-                double clicked_point_y = event.getSceneY() ;
-                int position = cardList.indexOf(card1);
+                int position = cardList.indexOf(card1); //position in the hand based on when it was added to cardlist
+
+                /*play event just simple print output with useful info*/
                 System.out.println("Player 1 played " + card1.suit +card1.rank +" from position "+position);
             });
         }
+        /*add the cardlist to the hand horizontal box object so the cards will be drawn*/
         hand.getChildren().addAll(cardList);
         pane_for_buttons.getChildren().addAll( button1, button2 ) ;
-
         pane_for_buttons.setAlignment( Pos.CENTER ) ;
-
         pane_for_buttons.setPadding( new Insets( 0, 0, 20, 0 ) ) ;
 
+        /*create a borderpane to allow some layout changes and add the hand to it*/
         BorderPane border_pane = new BorderPane() ;
-
         border_pane.setTop( pane_for_buttons ) ;
         border_pane.setBottom(hand);
+
+        /*add that borderpane to the main pane and set its position in it*/
         root_stack.getChildren().addAll( border_pane);
         root_stack.setLayoutX(350);
         root_stack.setLayoutY(600);
 
+        /*draw the window and scene*/
         stage.setScene( scene ) ;
         stage.show() ;
     }
