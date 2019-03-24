786b82ffa12ca6399839d18b54e7dc4a7910983a
oll096 <oll096@usask.ca>
2019-03-24 15:23:26 -0600

    Add Card, Firework, Hand, Table classes to the ViewTest

---
+++ HanabiSrcCode/main/Card.java
@@ -0,0 +1,137 @@
+/**
+ * The class representing a single Hanabi card.
+ */
+public class Card {
+
+    /**
+     * number rank of the card
+     */
+    private char number;
+
+    /**
+     * colour suit of the card
+     */
+    private char colour;
+
+    /**
+     * marker for whether the colour of the card is known
+     */
+    private Boolean colourKnown;
+
+    /**
+     * marker for whether the rank of the card is known
+     */
+    private Boolean numberKnown;
+
+
+    /**
+     * Purpose: Card constructor for other players' cards, takes color and number value
+     * and sets both colourKnown and numberKnown to false.
+     *
+     * @param colour the colour of the card.
+     * @param number the rank of the card.
+     */
+    public Card(char colour, char number) {
+
+        this.colour = colour;
+        this.number = number;
+
+        this.colourKnown = false;
+        this.numberKnown = false;
+    }
+
+    /**
+     * Purpose: Card constructor for the user cards,
+     * sets both colourKnown and numberKnown to false.
+     */
+    public Card() {
+
+        /* Assumes colour and number are instantiated to null. */
+        this.colour = 'u';
+        this.number = 'u';
+
+        this.colourKnown = false;
+        this.numberKnown = false;
+    }
+
+    /**
+     * Purpose: sets numberKnown to true, when a number is revealed to the card's owner
+     */
+    public void numberInformed() {
+        this.numberKnown = true;
+    }
+
+    /**
+     * Purpose: sets colourKnown to true, when a number is revealed to the card's owner
+     */
+    public void colourInformed() {
+        this.numberKnown = true;
+    }
+
+    /**
+     * Purpose: returns the rank of the card.
+     *
+     * @return the char representing the rank.
+     */
+    public char getNumber() {
+        return this.number;
+    }
+
+    /**
+     * Purpose: returns the colour of the card.
+     *
+     * @return the char representing the colour.
+     */
+    public char getColour() {
+        return this.colour;
+    }
+
+    /**
+     * Purpose: returns whether the colour of the card has been given as information.
+     *
+     * @return true if it has been informed, false otherwise.
+     */
+    public boolean checkColourKnown() {
+        return this.colourKnown;
+    }
+
+    /**
+     * Purpose: returns whether the number of the card has been given as information.
+     *
+     * @return true if it has been informed, false otherwise.
+     */
+    public boolean checkNumberKnown() {
+        return this.numberKnown;
+    }
+
+    /**
+     * Purpose: sets the number field of a users card after it has been revealed.
+     *
+     * @param number the char representing the rank of the card.
+     */
+    public void setNumber(char number) {
+        this.number = number;
+        this.numberInformed();
+    }
+
+    /**
+     * Purpose: sets the colour field of a users card after it has been revealed.
+     *
+     * @param colour the char representing the colour of the card.
+     */
+    public void setColour(char colour) {
+        this.colour = colour;
+        this.colourInformed();
+    }
+
+    /**
+     * Purpose: outputs a string representation of the card object
+     *
+     * @return a two character string.
+     */
+    public String toString() {
+        String out = Character.toString(this.colour);
+        out += Character.toString(this.number);
+        return out;
+    }
+}
---
+++ HanabiSrcCode/main/Table.java
@@ -0,0 +1,94 @@
+/**
+ * Class representing the all player hands in the game in the model.
+ */
+public class Table {
+    /**
+     * playerHands: an array of player hands, representing player seat numbers and turn order.
+     */
+    protected Hand[] playerHands;
+
+    /**
+     * Purpose: builds Table with the given number of player hands.
+     *
+     * @param players the number of players in the game.
+     */
+    public Table(int players) {
+        this.playerHands = new Hand[players];
+        int handSize;
+        if (4 <= players) {
+            handSize = 5;
+        } else {
+            handSize = 4;
+        }
+        for (Hand hand : this.playerHands) {
+            hand = new Hand(handSize);
+        }
+    }
+
+    /**
+     * Purpose: Adds a card with the given rank and suit to the chosen player's hand.
+     *
+     * @param playerID the player to receive the card.
+     * @param colour   the colour of the new card.
+     * @param number   the number of the new card.
+     */
+    public void giveCard(int playerID, char colour, char number) {
+        this.playerHands[playerID - 1].addCard(colour, number);
+    }
+
+    /**
+     * Purpose: Adds a card with the given rank and suit to the user's hand.
+     *
+     * @param playerID the player to receive the card.
+     */
+    public void giveCard(int playerID) {
+        this.playerHands[playerID - 1].addCard();
+    }
+
+    /**
+     * Purpose: Removes a card at the given position from the given player hand.
+     *
+     * @param playerID     the player hand to remove the card from.
+     * @param cardPosition the position of the card in the player's hand.
+     */
+    public void removeCard(int playerID, int cardPosition) {
+        this.playerHands[playerID - 1].removeCard(cardPosition - 1);
+    }
+
+    /**
+     * Purpose: Set the inform flags on the given player's cards.
+     *
+     * @param playerID the player who was informed.
+     * @param type     the type of the inform done.
+     * @param info     the information that was informed.
+     */
+    public void informCard(int playerID, String type, char info) {
+        if (type == "colour") {
+            this.playerHands[playerID - 1].informColour(info);
+        } else if (type == "number") {
+            this.playerHands[playerID - 1].informNumber(info);
+        } else {
+            //TODO: make exception class.
+            //throw new InvalidInformTypeException("Invalid inform type: " + type);
+        }
+    }
+
+    /**
+     * Purpose: Set the card information and inform flags on the user's cards.
+     *
+     * @param playerID the player who was informed.
+     * @param type     the type of the inform done.
+     * @param info     the information that was informed.
+     * @param index    the position of the card that was informed.
+     */
+    public void informCard(int playerID, String type, char info, int index) {
+        if (type == "colour") {
+            this.playerHands[playerID - 1].informColourUser(index, info);
+        } else if (type == "number") {
+            this.playerHands[playerID - 1].informNumberUser(index, info);
+        } else {
+            //TODO: make exception class.
+            //throw new InvalidInformTypeException("Invalid inform type: " + type);
+        }
+    }
+}
